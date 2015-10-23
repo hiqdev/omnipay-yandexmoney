fa5f24f7d37c44b295c6f90cd2bf2bca8284f65f
Andrii Vasyliev <sol@hiqdev.com>
2015-10-23 12:34:37 +0000

    inited

---
+++ src/Merchant.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * Yandex.Money plugin for PHP merchant library
+ *
+ * @link      https://github.com/hiqdev/php-merchant-yandexmoney
+ * @package   php-merchant-yandexmoney
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015, HiQDev (https://hiqdev.com/)
+ */
+
+namespace hiqdev\php\merchant\yandexmoney;
+
+class Merchant extends \hiqdev\php\merchant\Merchant
+{
+    protected static $_defaults = [
+        'name'        => 'yandexmoney',
+        'label'       => 'Yandex.Money',
+        'actionUrl'   => 'https://money.yandex.ru/quickpay/confirm.xml',
+    ];
+
+    public function getInputs()
+    {
+        return [
+            'receiver'          => $this->purse,
+            'label'             => $this->username,
+            'FormComment'       => 'Оплата товара/услуги',
+            'short-dest'        => 'Оплата товара/услуги',
+            'writable-targets'  => 'false',
+            'writable-sum'      => 'false',
+            'comment-needed'    => 'false',
+            'quickpay-form'     => 'shop',
+            'fio'               => '1',
+            'mail'              => '1',
+            'targets'           => $this->username.'_'.$this->total,
+            'sum'               => $this->total,
+        ];
+    }
+
+    public function validateConfirmation($data)
+    {
+        if ($data['test_notification']) {
+            return 'Test request';
+        }
+        $hash = join('&', [
+            $data['notification_type'], $data['operation_id'], $data['amount'], $data['currency'],
+            $data['datetime'], $data['sender'], $data['codepro'], $this->secret, $data['label'],
+        ]);
+        if (hash('sha1', $hash) != $data['sha1_hash']) {
+            return 'Wrong hash';
+        }
+        $this->mset([
+            'from'  => $data['sender'],
+            'sum'   => $data['amount'],
+            'txn'   => $data['operation_id'],
+            'time'  => $data['datetime'],
+        ]);
+        return null;
+    }
+}
