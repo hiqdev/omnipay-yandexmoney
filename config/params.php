7ebbdfd7bcb4d05edeb2c7f0f3cd88f47c9ac278
Yurii Myronchuk <bladeroot@gmail.com>
2020-03-18 11:25:00 +0000

    add required data from client

---
+++ config/params.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Generalization over Omnipay and Payum
+ *
+ * @link      https://github.com/hiqdev/php-merchant
+ * @package   php-merchant
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2020, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'php.nerchant.yandex.request.sender.need-fio' => true,
+    'php.nerchant.yandex.request.sender.need-email' => true,
+    'php.nerchant.yandex.request.sender.need-phone' => false,
+    'php.nerchant.yandex.request.sender.need-address' => false,
+];
+
